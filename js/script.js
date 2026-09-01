6d67491d829c1e4f06b43f608012880dab8cc2a3
Arunkumar2706-dot <marun4201@gmail.com>
2026-09-01 23:31:42 +0530

    Complete structural redesign of AGA Groups website

--- js/script.js
+++ js/script.js
@@ -376,9 +376,9 @@ function initAOS() {
   if (typeof AOS !== 'undefined') {
     AOS.init({
       once: true,
-      duration: 800,
-      easing: 'ease-out-cubic',
-      offset: 80,
+      duration: 600,
+      easing: 'ease-out-quad',
+      offset: 50,
     });
   }
 }
